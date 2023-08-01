97609f9dccad918a8b38365d918ee7e066fba55f
cem256 <cemo99@gmail.com>
2023-08-01 17:33:16 +0300

    Day31_WordScramble

---
+++ Day31_WordScramble/Day31_WordScramble/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Day31_WordScramble
+//
+//  Created by cem on 1.08.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Day31_WordScramble/Day31_WordScramble/Day31_WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day31_WordScrambleApp.swift
+//  Day31_WordScramble
+//
+//  Created by cem on 1.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Day31_WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
