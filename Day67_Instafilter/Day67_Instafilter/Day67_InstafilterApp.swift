31146910ec4747450bf8cb524a25e2a72c63cf8d
cem256 <cemo99@gmail.com>
2023-08-10 13:40:33 +0300

    Day67_Instafilter

---
+++ Day67_Instafilter/Day67_Instafilter/Day67_InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day67_InstafilterApp.swift
+//  Day67_Instafilter
+//
+//  Created by cem on 10.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Day67_InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
