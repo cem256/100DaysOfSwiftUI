7f86381fc1c966bd66453e7066525d4bbd393602
cem256 <cemo99@gmail.com>
2023-08-17 17:27:51 +0300

    Day94_LayoutAndGeometry

---
+++ Day94_LayoutAndGeometry/Day94_LayoutAndGeometry/Day94_LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day94_LayoutAndGeometryApp.swift
+//  Day94_LayoutAndGeometry
+//
+//  Created by cem on 17.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Day94_LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
